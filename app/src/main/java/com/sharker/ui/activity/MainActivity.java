f49829bfbf25296a71389205d8b78655213e0a22
liqingyi <liqingyi@outlook.com>
2017-04-09 08:22:20 +0800

    导航栏

--- app/src/main/java/com/sharker/ui/activity/MainActivity.java
+++ app/src/main/java/com/sharker/ui/activity/MainActivity.java
@@ -1,6 +1,11 @@
 package com.sharker.ui.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 
 import com.sharker.R;
@@ -8,9 +13,15 @@ import com.sharker.models.FirstHand;
 import com.sharker.models.data.AdBanner;
 import com.sharker.models.data.CourseData;
 import com.sharker.network.SharkerParams;
+import com.sharker.ui.fragment.CourseFragment;
+import com.sharker.ui.widget.IconTextView;
 
 import org.xutils.common.Callback;
 import org.xutils.x;
+
+import me.majiajie.pagerbottomtabstrip.NavigationController;
+import me.majiajie.pagerbottomtabstrip.PageBottomTabLayout;
+import me.majiajie.pagerbottomtabstrip.item.BaseTabItem;
 
 public class MainActivity extends BaseActivity {
 
@@ -18,6 +29,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
         FirstHand.getInstance();
         if (FirstHand.isHost()) {
             listBanner();
@@ -27,6 +39,46 @@ public class MainActivity extends BaseActivity {
         } else {
             firstHand();
         }
+    }
+
+    void initView(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        PageBottomTabLayout tab = (PageBottomTabLayout) findViewById(R.id.tab);
+
+        //注意这里调用了custom()方法
+        final NavigationController navigationController = tab.custom()
+                .addItem(newItem(R.mipmap.common_course_unselected,R.mipmap.common_course_selected,"学习"))
+                .addItem(newItem(R.mipmap.common_study_unselected,R.mipmap.common_study_selected,"课程"))
+                .addItem(newItem(R.mipmap.common_mine_unselected,R.mipmap.common_mine_selected,"我的"))
+                .build();
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return CourseFragment.newInstance("","");
+            }
+
+            @Override
+            public int getCount() {
+                return navigationController.getItemCount();
+            }
+        });
+        navigationController.setupWithViewPager(viewPager);
+    }
+
+
+
+
+    //创建一个Item
+    private BaseTabItem newItem(int drawable, int checkedDrawable, String text){
+        IconTextView normalItemView = new IconTextView(this);
+        normalItemView.initialize(drawable,checkedDrawable,text);
+        normalItemView.setTextDefaultColor(Color.GRAY);
+        normalItemView.setTextCheckedColor(0xFF009688);
+        return normalItemView;
     }
 
     public void firstHand() {
